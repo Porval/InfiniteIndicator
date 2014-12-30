d919f9f9fe32a7b6e9bf267db3ca59c120002f2a
jiangpeng <jiangpeng@douban.com>
2014-12-30 12:12:07 +0800

    new demo list activity

---
+++ demo/src/main/java/com/jp/infinite/indicator/demo/DemoListActivity.java
@@ -0,0 +1,13 @@
+package com.jp.infinite.indicator.demo;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+
+
+public class DemoListActivity extends ListActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}

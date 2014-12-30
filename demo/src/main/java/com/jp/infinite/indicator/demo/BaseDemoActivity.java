8174e1a33f5359894416981b4e962a5c7fd1009b
jiangpeng <jiangpeng@douban.com>
2014-12-30 14:47:33 +0800

    create base demo activity and demo fragment

---
+++ demo/src/main/java/com/jp/infinite/indicator/demo/BaseDemoActivity.java
@@ -0,0 +1,75 @@
+package com.jp.infinite.indicator.demo;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import com.jp.inifinite.InfinitePagerAdapterWrapper;
+import com.jp.inifinite.InfinitePagerView;
+import com.jp.inifinite.indicator.InfinitePageIndicator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class BaseDemoActivity extends FragmentActivity {
+
+    private InfinitePagerView mInfinitePagerView;
+
+    private InfinitePageIndicator mInfinitePageIndicator;
+
+    private DemoPagerAdapter mDemoPageAdapter;
+
+    private String mTitle;
+
+    //在onStart中调用
+    public abstract InfinitePagerView onCreateInfinitePagerView();
+
+    //在onStart中调用
+    public abstract InfinitePageIndicator onCreateInfinitePageIndicator();
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mInfinitePagerView = onCreateInfinitePagerView();
+        mInfinitePageIndicator = onCreateInfinitePageIndicator();
+
+        mDemoPageAdapter = new DemoPagerAdapter(getSupportFragmentManager());
+        mDemoPageAdapter.init();
+
+        mInfinitePagerView.setAdapter(new InfinitePagerAdapterWrapper(mDemoPageAdapter));
+        mInfinitePageIndicator.setViewPager(mInfinitePagerView);
+    }
+
+    private class DemoPagerAdapter extends FragmentPagerAdapter {
+        private List<Fragment> mFragmentList;
+
+        public DemoPagerAdapter(FragmentManager fm) {
+            super(fm);
+            mFragmentList = new ArrayList<Fragment>();
+        }
+
+        public void init() {
+            for (int index = 0; index < 5; index++) {
+                mFragmentList.add(
+                        new DemoFragment()
+                                .title(mTitle)
+                );
+            }
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+    }
+}
---
+++ demo/src/main/java/com/jp/infinite/indicator/demo/DemoFragment.java
@@ -0,0 +1,33 @@
+package com.jp.infinite.indicator.demo;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.jp.inifinite.indicator.demo.R;
+
+public class DemoFragment extends Fragment {
+
+    private String mTitleStr;
+
+    public DemoFragment title(String title) {
+        mTitleStr = title;
+        return this;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.frag_demo, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        TextView title = (TextView) view.findViewById(R.id.title);
+        if (mTitleStr != null && !mTitleStr.isEmpty() && title != null) {
+            title.setText(mTitleStr);
+        }
+    }
+}
